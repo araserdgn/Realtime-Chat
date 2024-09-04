a1f63e66e36b9dfce5e8d8661f4dec532aeecdcb
araserdgn <113991303+araserdgn@users.noreply.github.com>
2024-09-05 00:56:47 +0300

    I filled UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,20 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $users = [
+            [
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('password'),
+            ],
+            [
+                'name' => 'Aras',
+                'email' => 'aras@gmail.com',
+                'password' => Hash::make('password'),
+            ],
+
+            ];
+
+        User::insert($users);
     }
 }
