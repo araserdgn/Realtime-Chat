ed7d064569aba09b7d13ea0bee5a11819d0da9db
araserdgn <113991303+araserdgn@users.noreply.github.com>
2024-09-04 21:20:35 +0300

    I created Room/Show.vue , RoomResource and I updated RoomControler

---
+++ resources/js/Pages/Room/Show.vue
@@ -0,0 +1,38 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+
+defineProps({
+    room: {
+        type:Object,
+        required: true,
+    },
+
+    // messages: {
+    //     type:Array,
+    //     required:true,
+    // },
+})
+
+</script>
+
+<template>
+    <Head title="ChatPage" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Message Area</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        {{ room }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+
+</template>
