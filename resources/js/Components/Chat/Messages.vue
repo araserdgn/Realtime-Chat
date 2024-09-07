46bc8e9b065bb1627246e5e660fcce16e6862c10
araserdgn <113991303+araserdgn@users.noreply.github.com>
2024-09-07 15:04:19 +0300

    I used IntersectionObserver to follow messages

--- resources/js/Components/Chat/Messages.vue
+++ resources/js/Components/Chat/Messages.vue
@@ -1,6 +1,28 @@
-
 <script setup>
-import { useMessagesStore } from '@/Store/useMessagesStore';
+import { useMessagesStore } from "@/Store/useMessagesStore";
+
+import { useIntersectionObserver } from '@vueuse/core'
+import { ref } from "vue";
+
+const target = ref(null)
+const targetIsVisible = ref(false)
+
+
+const props = defineProps({
+    room: {
+        type: Object,
+        required: true,
+    },
+});
+
+const { stop } = useIntersectionObserver(
+    target,
+        ([{ isIntersecting }], observerElement) => {
+            if (isIntersecting) {
+                console.log("İs interesting");
+            }
+        },
+)
 
 const messagesStore = useMessagesStore();
 
@@ -9,24 +31,55 @@ const messagesStore = useMessagesStore();
 <template>
     <main id="page-content" class="absolute inset-0">
         <div
-            class="container mx-auto space-y-6 space-y-reverse px-4 py-24 lg:p-8 lg:pb-28 xl:max-w-7xl flex flex-col-reverse h-full overflow-y-auto">
-            <div v-for="message in messagesStore.allMessages" :key="message.id"
-                class="flex w-5/6 flex-col gap-2 lg:w-2/3 xl:w-1/3 items-start">
-                <p v-if="message.user.id != $page.props.auth.user.id" class="text-sm font-medium text-slate-500">
+            class="container mx-auto space-y-6 space-y-reverse px-4 py-24 lg:p-8 lg:pb-28 xl:max-w-7xl flex flex-col-reverse h-full overflow-y-auto"
+        >
+            <div
+                v-for="message in messagesStore.allMessages"
+                :key="message.id"
+                class="flex w-5/6 flex-col gap-2 lg:w-2/3 xl:w-1/3"
+                :class="{
+                    'items-start': message.user.id != $page.props.auth.user.id,
+                    'items-end ms-auto':
+                        message.user.id == $page.props.auth.user.id,
+                }"
+            >
+                <p
+                    v-if="message.user.id != $page.props.auth.user.id"
+                    class="text-sm font-medium text-slate-500"
+                >
                     {{ message.user.name }}
                 </p>
-
-                <div class="rounded-2xl px-5 py-3 bg-gray-100">
-                    <p class="font-semibold">
+                <div
+                    class="rounded-2xl px-5 py-3 bg-gray-100"
+                    :class="{
+                        'bg-indigo-600 text-white rounded-br-none':
+                            message.user.id == $page.props.auth.user.id,
+                        'rounded-tl-none':
+                            message.user.id != $page.props.auth.user.id,
+                    }"
+                >
+                    <p
+                        class="font-semibold"
+                        :class="{
+                            'text-white':
+                                message.user.id == $page.props.auth.user.id,
+                            'text-slate-500':
+                                message.user.id != $page.props.auth.user.id,
+                        }"
+                    >
                         {{ message.content }}
                     </p>
                 </div>
-
-                <p class="text-xs font-medium text-slate-500">
+                <p
+                    class="text-xs font-medium text-slate-500"
+                    :class="{
+                        'text-right':
+                            message.user.id == $page.props.auth.user.id,
+                    }"
+                >
                     {{ message.created_at }}
                 </p>
             </div>
-
             <div ref="target" class="translate-y-20"></div>
         </div>
     </main>
