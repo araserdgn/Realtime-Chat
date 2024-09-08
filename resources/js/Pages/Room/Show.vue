646e3606028beba11d4055b4ac61c952e0f70f03
araserdgn <113991303+araserdgn@users.noreply.github.com>
2024-09-08 13:00:56 +0300

    I edited useUsersStore and Shoe.vue for keeping online users in sync

--- resources/js/Pages/Room/Show.vue
+++ resources/js/Pages/Room/Show.vue
@@ -31,9 +31,9 @@ channel.listen("MessageCreated", (e) => {
 
     messagesStore.pushMessage(e);
 })
-.here((users) => [
-    usersStore.setUsers(users)
-]);
+.here((users) => usersStore.setUsers(users))
+.joining((user) => usersStore.addUser(user))
+.leaving((user) => usersStore.removeUser(user))
 
 
 // console.log(Echo.socketId());
--- resources/js/Store/useUsersStore.js
+++ resources/js/Store/useUsersStore.js
@@ -11,6 +11,20 @@ export const useUsersStore = defineStore("users", {
             this.users = users;
         },
 
+        addUser(user) {
+            if (
+                typeof this.users.find((u) => u.id === user.id) !== "undefined"
+            ) {
+                return;
+            }
+
+            this.users.push(user);
+        },
+
+        removeUser(user) {
+            this.users = this.users.filter((u) => u.id !== user.id);
+        },
+
     },
 
     getters: {
